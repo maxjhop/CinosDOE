725765de41ed38c36f1bc80bc193a288713cc0ae
maxjhop <maxjhop99@gmail.com>
2021-04-24 16:50:04 -0700

    spells/enemy

--- CinosDOE/Assets/Scripts/SpellScript.cs
+++ CinosDOE/Assets/Scripts/SpellScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpellScript : MonoBehaviour
 {
+    public LayerMask playerMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,11 @@ public class SpellScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            Destroy(gameObject);
     }
 }
