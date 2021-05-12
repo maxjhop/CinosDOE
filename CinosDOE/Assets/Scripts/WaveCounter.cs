ac877d5bffdbbd16f45dabee3c592ce51e2a5f87
River Veek <riverv@uoregon.edu>
2021-05-11 19:43:42 -0700

    Wave text now works as expected; added shop keeper

--- CinosDOE/Assets/Scripts/WaveCounter.cs
+++ CinosDOE/Assets/Scripts/WaveCounter.cs
@@ -6,39 +6,38 @@ using UnityEngine.UI;
 public class WaveCounter : MonoBehaviour
 {
     public Text waveNumber;
-    private int num = 1;
-    //private int duration = 6;
-    //public bool testFlag = false;
-    public bool beginWave = true;
+    private int num =0;
+    private float elapsed = 0f;
+    public bool showingText = false;
+    public int MAX_WAVES = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        waveNumber.text += num.ToString();
+        UpdateNum();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        // only show text for a few seconds at the start of each wave
-        if(beginWave)
+        if (showingText && (Time.time - elapsed) > 4)
         {
-            //waveNumber.enabled = false;
-            //beginWave = false;
+            waveNumber.enabled = false;
         }
-        if (testFlag)
+        if(num > MAX_WAVES)
         {
-            UpdateNum();
-            testFlag = false;
+            waveNumber.text = "LEVEL COMPLETE!";
+            showingText = false;
         }
-        */
     }
 
     public void UpdateNum()
     {
         Debug.Log("in UpdateNum()");
         num += 1;
+        waveNumber.enabled = true;
         waveNumber.text = "Wave " + num.ToString();
+        elapsed = Time.time;
+        showingText = true;
     }
 }
