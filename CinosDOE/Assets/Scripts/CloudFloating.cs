e72aac48ccc5914a601c0c87ad1e5bd9a164ed54
River Veek <riverv@uoregon.edu>
2021-05-23 16:55:50 -0700

    Trashed bobbing cloud idea; added lighting to storm clouds

--- CinosDOE/Assets/Scripts/CloudFloating.cs
+++ CinosDOE/Assets/Scripts/CloudFloating.cs
@@ -5,25 +5,52 @@ using UnityEngine;
 public class CloudFloating : MonoBehaviour
 {
     private GameObject cloud;
-    private float startingHeight;
+    //private float startingHeight;
+    private Vector3 startingPosition;
+    private Vector3 endingPosition;
     public float maxShift = 5f;
-    public float increment = .2f;
+    private float startingTime;
+    private float totalDistance;
+    public float bobbingSpeed = .75f;
+    // public float increment = .2f;
 
     // Start is called before the first frame update
     void Start()
     {
         cloud = this.gameObject;
-        startingHeight = this.transform.position.y;
+        //startingHeight = this.transform.position.y;
+        startingPosition = transform.position;
+        endingPosition = new Vector3(startingPosition.x, startingPosition.y + maxShift, startingPosition.z);
+        //Debug.Log(startingPosition);
+        //Debug.Log(endingPosition);
+        startingTime = Time.time;
+        totalDistance = Vector3.Distance(startingPosition, endingPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float curPos = cloud.transform.position.y;
-        curPos += increment;
+        // the following code was attempting to make the clouds bob up and down \
+        // but it wasn't working and I don't think it's worth it
+        /*
+        float elapsedDistance = (Time.time - startingTime) * bobbingSpeed;
+        float curDistance = elapsedDistance / totalDistance;
+        if (curDistance == 1)
+        {
+            startingTime = Time.time;
+            Vector3 temp = startingPosition;
+            startingPosition = endingPosition;
+            endingPosition = temp;
+            totalDistance = Vector3.Distance(startingPosition, endingPosition);
+
+        }
+        //float curPos = cloud.transform.position.y;
+        //curPos += increment;
         // FIXME
-        // transform.position = Vector3.Lerp(transform.position, )
-        if (curPos < curPos + maxShift)
+        transform.position = Vector3.Lerp(startingPosition, endingPosition, curDistance);
+        //Debug.Log("updating cloud pos");
+        
+        if (curPos > curPos + maxShift)
         {
             increment *= -1;
         }
@@ -31,5 +58,6 @@ public class CloudFloating : MonoBehaviour
         {
             increment *= -1;
         }
+        */
     }
 }
